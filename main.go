4a7a1ae8418dd589fea75d27c332da5268de8922
sigsant <sigfridsantamera@hotmail.com>
2020-10-30 03:18:12 +0100

    Limpieza de codigo

--- main.go
+++ main.go
@@ -1,11 +1,3 @@
-/*
-	Create a program that will allow you to enter events organizable by hour.
-	There must be menu options of some form, and you must be able to easily edit, add, and delete events
-	without directly changing the source code.
-
-	source: https://www.reddit.com/r/dailyprogrammer/comments/pihtx/intermediate_challenge_1/
-*/
-
 package main
 
 import (
