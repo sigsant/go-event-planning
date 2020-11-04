e431bd018b5e340a5611cf2547fa4fc044b76a78
sigsant <sigfridsantamera@hotmail.com>
2020-11-04 22:46:00 +0100

    Factorizar procesamiento CSV

--- modules/csvLine/csvline.go
+++ modules/csvLine/csvline.go
@@ -1,18 +1,41 @@
 package csvline
 
 import (
+	"encoding/csv"
+	"errors"
 	"fmt"
+	"log"
+	"os"
 )
 
-type CsvLine struct {
+type Csvline struct {
 	Hora   string
 	Nombre string
+}
+
+var (
+	errFicheroNoExistente = errors.New("ERROR: El fichero no existe")
+	errSinLineas          = errors.New("ERROR: Imposible leer líneas")
+)
+
+func ProcesarArchivo(fichero *string) [][]string {
+	file, err := os.Open(*fichero)
+	if err != nil {
+		log.Fatal(errFicheroNoExistente)
+	}
+	defer file.Close()
+
+	lineas, err := csv.NewReader(file).ReadAll()
+	if err != nil {
+		log.Fatal(errSinLineas)
+	}
+	return lineas
 }
 
 // MostrarEvento muestra linea a linea los eventos guardados en el fichero csv.
 func MostrarEvento(lineas [][]string) {
 	for _, linea := range lineas {
-		data := CsvLine{
+		data := Csvline{
 			Hora:   linea[0],
 			Nombre: linea[1],
 		}
